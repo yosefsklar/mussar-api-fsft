dc72a52b8addbb79f902a7ae355f43f5dce7ece4
yosefsklar <yosef.sklar@gmail.com>
2025-12-14 13:30:49 -0500

    add new images

--- frontend/src/components/Common/Navbar.tsx
+++ frontend/src/components/Common/Navbar.tsx
@@ -1,7 +1,7 @@
 import { Flex, Image, useBreakpointValue } from "@chakra-ui/react"
 import { Link } from "@tanstack/react-router"
 
-import Logo from "/assets/images/fastapi-logo.svg"
+import Logo from "/assets/images/mussar_app_logo.png"
 import UserMenu from "./UserMenu"
 
 function Navbar() {
