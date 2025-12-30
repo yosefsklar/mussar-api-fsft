63f22c40c3ae4736a22a242861a0bc7bd578d969
yosefsklar <yosef.sklar@gmail.com>
2025-12-30 16:19:41 -0500

    made middot a flame icon

--- frontend/src/components/Common/SidebarItems.tsx
+++ frontend/src/components/Common/SidebarItems.tsx
@@ -1,9 +1,10 @@
 import { Box, Flex, Icon, Text } from "@chakra-ui/react"
 import { useQueryClient } from "@tanstack/react-query"
 import { Link as RouterLink } from "@tanstack/react-router"
+
 import { FiBriefcase, FiHome, FiSettings, FiUsers } from "react-icons/fi"
-import { LuSpeech } from "react-icons/lu";
-import { RiMentalHealthLine } from "react-icons/ri"
+import { IoFlameOutline } from "react-icons/io5";
+import { LuSpeech } from "react-icons/lu"
 import type { IconType } from "react-icons/lib"
 
 import type { UserPublic } from "@/client"
@@ -11,7 +12,7 @@ import type { UserPublic } from "@/client"
 const items = [
   { icon: FiHome, title: "Dashboard", path: "/" },
   { icon: FiBriefcase, title: "Items", path: "/items" },
-  { icon: RiMentalHealthLine, title: "Middot", path: "/middot" },
+  { icon: IoFlameOutline , title: "Middot", path: "/middot" },
   { icon: LuSpeech, title: "Reminder Phrases", path: "/reminder-phrases" },
   { icon: FiSettings, title: "User Settings", path: "/settings" },
 ]
