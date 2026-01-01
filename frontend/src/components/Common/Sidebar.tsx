c6a4b623f2c3aa5361c72aa5e410617f54b08371
yosefsklar <yosef.sklar@gmail.com>
2026-01-01 13:58:28 -0500

    keep beige sidebar

--- frontend/src/components/Common/Sidebar.tsx
+++ frontend/src/components/Common/Sidebar.tsx
@@ -80,7 +80,7 @@ const Sidebar = () => {
       <Box
         display={{ base: "none", md: "flex" }}
         position="sticky"
-        bg="g.subtle"
+        bg="#EAE8CF"
         top={0}
         minW="xs"
         h="100vh"
